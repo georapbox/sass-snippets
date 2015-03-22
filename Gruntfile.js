eef87ed0d67f3d630e2230bc7b346d81c1c70717
georapbox <georapbox@gmail.com>
2015-03-22 16:22:04 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+            dev: {
+                options: {
+                    sourcemap: true,
+                    trace: true,
+                    sassDir: 'examples/sass',
+                    cssDir: 'examples/css'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['*.css'],
+            }
+        },
+    });
+
+    /* ------------- Load Grunt Plugins ------------- */
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    /* --------------- Register Tasks --------------- */
+    grunt.registerTask('watch:dev', function () {
+        var config = {
+            sass: {
+                files: ['examples/sass/*.scss'],
+                tasks: ['compass:dev']
+            }
+        };
+
+        grunt.config('watch', config);
+        grunt.task.run('watch');
+    });
+};
